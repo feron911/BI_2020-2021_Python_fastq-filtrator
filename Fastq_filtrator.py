01d6bf5624cbf8d750f98dd315616d8b3bd6e26e
feron911 <63519699+feron911@users.noreply.github.com>
2021-02-05 17:28:25 +0300

    Add files via upload

--- Fastq_filtrator.py
+++ Fastq_filtrator.py
@@ -1,10 +1,10 @@
-import os, sys
+import os, sys, re
 
 args = sys.argv
 
 minlen = False
 keepfil = False
-gccount = False
+gcfilt = False
 
 if 'C:\\' in args[-1]:
     fqfile = args[-1]
@@ -13,24 +13,27 @@ else:
 
 if '--min-length' in args:
     minlen = True
-    min_read_len = args[args.index('--min-length') + 1]
+    if args[args.index('--min-length') + 1].isdigit():
+        min_read_len = int(args[args.index('--min-length') + 1])
 
 if '--output_base_name' in args:
-    outname = args[args.index('--output_base_name') + 1]
+    outname = args[args.index('--output_base_name') + 1] + '.fastq'
 else:
     for i in range(-1, -len(args[-1]), -1):
         if args[-1][i] == '.':
+            outname = args[-1][:i]
             break
-    outname = args[-1][:-6]
+    else:
+        outname = args[-1]
 
 if '--keep_filtered' in args:
     keepfil = True
     outname_p = outname + '__passed.fastq'
     outname_f = outname + '__failed.fastq'
-else:
-    outname = outname
+
 
 if '--gc_bounds' in args:
+    gcfilt = True
     gccount = [int(args[args.index('--gc_bounds') + 1])]
     if args[args.index('--gc_bounds') + 2].isdigit():
         gccount += [int(args[args.index('--gc_bounds') + 2])]
@@ -42,23 +45,33 @@ def write_outfq(name, read):
 with open(fqfile, 'r') as fqsample:
     i = 0
     Read = []
-    k = 0
+    failed = False
     for line in fqsample:
         Read += [line]
         i += 1
         if i == 2:
             if minlen:
-                #if
-                pass
-            pass
+                if len(Read[1]) < min_read_len:
+                    if keepfil:
+                        failed = True
+            if gcfilt:
+                gc_perc = (Read[1].count("G") + Read[1].count("C")) / len(Read[1]) * 100
+                if len(gccount) == 1:
+                    if gc_perc < gccount[0]:
+                        failed = True
+                if len(gccount) == 2:
+                    if gc_perc < gccount[0] or gc_perc > gccount[1]:
+                        failed = True
         if i == 4:
-
-                print(Read)
-                for i in range(4):
-                    write_outfq(outname, Read[i])
+            if failed and keepfil:
+                for j in range(4):
+                    write_outfq(outname_f, Read[j])
+            elif keepfil:
+                for j in range(4):
+                    write_outfq(outname_p, Read[j])
+            elif not failed:
+                for j in range(4):
+                    write_outfq(outname, Read[j])
             i = 0
             Read = []
-        k += 1
-        if k > 4:
-            break
-
+            failed = False
